7dfc5c55efc097edd8bfdd435eb22e7edaeda9fe
Webudvikler Hovedforløb <mich536x@elev.techcollege.dk>
2022-12-16 11:51:33 +0100

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import { v4 as uuidv4 } from "uuid";
+import "./App.css";
+
+const initialTodo = { id: uuidv4(), title: "", description: "", finished: false };
 
 function App() {
+  const [todos, setTodos] = useState([]);
+  const [state, setState] = useState(initialTodo);
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+
+    setState((prevState) => ({ ...prevState, [name]: value }));
+  };
+
+  const addTodo = () => {
+    setTodos((prev) => [...prev, state]);
+    setState(initialTodo);
+  };
+
+  const deleteTodo = ({ id }) => {
+    setTodos((prev) => prev.filter((todo) => todo.id === id));
+  };
+
+  const { title, description } = state;
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <input type="text" name="title" value={title} onChange={handleChange} placeholder="title" />
+      <input type="text" name="description" value={description} onChange={handleChange} placeholder="description" />
+
+      <button onClick={addTodo}>add todo</button>
+
+      <ul style={{ listStyleType: "none" }}>
+        {todos.map((todo) => (
+          <li key={todo.id} onClick={() => deleteTodo(todo.id)}>
+            <div>
+              <span>Title: </span>
+              {todo.title}
+            </div>
+
+            <div>
+              <span>Beskrivelse:</span>
+              {todo.description}
+            </div>
+          </li>
+        ))}
+      </ul>
+    </>
   );
 }
 
